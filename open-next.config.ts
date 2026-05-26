55aad1f1b2b24dfa22fe73e7b0dfe5a0db29d2ad
cryptohardcore <133330042+cryptohardcore@users.noreply.github.com>
2026-05-26 17:25:26 +0700

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,18 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
 
-export default defineCloudflareConfig({})
+export default defineCloudflareConfig({
+  default: {
+    minify: true,
+  },
+  // This extracts the heavy Payload CMS dashboard routes out of the main worker
+  functions: {
+    payloadAdmin: {
+      routes: [
+        'app/(payload)/api/[...slug]/route',
+        'app/admin/[[...segments]]/page'
+      ],
+      minify: true,
+    }
+  }
+})
